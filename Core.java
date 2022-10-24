6dccfd223204eb3549ea37732775cab204b5102e
Matthew <mz.liu@mail.utoronto.ca>
2022-10-23 23:27:39 -0400

    Outlined the plan of the algorithm

---
+++ Core.java
@@ -0,0 +1,13 @@
+public class Core {
+    private final int[][] board = new int[9][9];
+
+    // check row
+    private boolean checkRow(int value, int x, int y){
+
+    }
+
+    // check column
+    private boolean checkColumn(int value, int x, int y){
+
+    }
+}
